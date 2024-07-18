c30e068f7a21bfd348fea42610db6fc0961fb498
igalshilman <igal@restate.dev>
2024-07-18 10:02:09 +0200

    Update nix-shell to include rust

--- shell.nix
+++ shell.nix
@@ -7,7 +7,27 @@
     python3Packages.pip
     python3Packages.virtualenv
     just
+
+		# rust
+		rustup
+    clang
+    llvmPackages.bintools
+    protobuf
+    cmake
+		liburing
+		pkg-config
   ]);
+
+	RUSTC_VERSION =
+	    builtins.elemAt
+	    (builtins.match
+	     ".*channel *= *\"([^\"]*)\".*"
+	     (pkgs.lib.readFile ./rust-toolchain.toml)
+	    )
+	    0;
+
+  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+
   runScript = ''
     bash
   '';
