db238c62b1752a11796ea0b33dbd69c45429105a
Francesco Guardiani <francescoguard@gmail.com>
2025-02-20 12:43:41 +0100

    Protocol V4 (#33)

--- python/restate/server_context.py
+++ python/restate/server_context.py
@@ -13,7 +13,7 @@
 
 from datetime import timedelta
 import inspect
-from typing import Any, Awaitable, Callable, Dict, List, Optional, TypeVar, cast
+from typing import Any, Awaitable, Callable, Dict, List, Optional, TypeVar
 import typing
 import traceback
 
@@ -22,8 +22,8 @@ from restate.exceptions import TerminalError
 from restate.handler import Handler, handler_from_callable, invoke_handler
 from restate.serde import BytesSerde, JsonSerde, Serde
 from restate.server_types import Receive, Send
-from restate.vm import Failure, Invocation, NotReady, SuspendedException, VMWrapper, RunRetryConfig
-
+from restate.vm import Failure, Invocation, NotReady, SuspendedException, VMWrapper, RunRetryConfig, CANCEL_HANDLE # pylint: disable=line-too-long
+from restate._internal import PyDoProgressAnyCompleted, PyDoProgressReadFromInput, PyDoProgressExecuteRun, PyDoProgressCancelSignalReceived # pylint: disable=import-error,no-name-in-module,line-too-long
 
 T = TypeVar('T')
 I = TypeVar('I')
@@ -50,7 +50,7 @@ class ServerDurablePromise(DurablePromise):
     def value(self) -> Awaitable[Any]:
         vm: VMWrapper = self.server_context.vm
         handle = vm.sys_get_promise(self.name)
-        coro =  self.server_context.create_poll_coroutine(handle)
+        coro =  self.server_context.create_poll_or_cancel_coroutine(handle)
         serde = self.serde
         assert serde is not None
 
@@ -65,18 +65,18 @@ class ServerDurablePromise(DurablePromise):
         assert self.serde is not None
         value_buffer = self.serde.serialize(value)
         handle = vm.sys_complete_promise_success(self.name, value_buffer)
-        return self.server_context.create_poll_coroutine(handle)
+        return self.server_context.create_poll_or_cancel_coroutine(handle)
 
     def reject(self, message: str, code: int = 500) -> Awaitable[None]:
         vm: VMWrapper = self.server_context.vm
         py_failure = Failure(code=code, message=message)
         handle = vm.sys_complete_promise_failure(self.name, py_failure)
-        return self.server_context.create_poll_coroutine(handle)
+        return self.server_context.create_poll_or_cancel_coroutine(handle)
 
     def peek(self) -> Awaitable[Any | None]:
         vm: VMWrapper = self.server_context.vm
         handle = vm.sys_peek_promise(self.name)
-        coro =  self.server_context.create_poll_coroutine(handle)
+        coro =  self.server_context.create_poll_or_cancel_coroutine(handle)
         serde = self.serde
         assert serde is not None
 
@@ -109,6 +109,7 @@ class ServerInvocationContext(ObjectContext):
         self.attempt_headers = attempt_headers
         self.send = send
         self.receive = receive
+        self.run_coros_to_execute: dict[int, Awaitable[typing.Union[bytes | Failure]]] = {}
 
     async def enter(self):
         """Invoke the user code."""
@@ -125,8 +126,8 @@ class ServerInvocationContext(ObjectContext):
         except SuspendedException:
             pass
         except Exception as e:
-            fmt = '\n'.join(traceback.format_exception(e))
-            self.vm.notify_error(fmt)
+            stacktrace = '\n'.join(traceback.format_exception(e))
+            self.vm.notify_error(repr(e), stacktrace)
             raise e
 
     async def leave(self):
@@ -166,9 +167,8 @@ class ServerInvocationContext(ObjectContext):
             'more_body': False,
         })
 
-
-    async def create_poll_coroutine(self, handle) -> bytes | None:
-        """Create a coroutine to poll the handle."""
+    async def take_and_send_output(self):
+        """Take output from state machine and send it"""
         output = self.vm.take_output()
         if output:
             await self.send({
@@ -176,10 +176,36 @@ class ServerInvocationContext(ObjectContext):
                 'body': bytes(output),
                 'more_body': True,
             })
-        self.vm.notify_await_point(handle)
+
+    def must_take_notification(self, handle):
+        """Take notification, which must be present"""
+        res = self.vm.take_notification(handle)
+        if isinstance(res, NotReady):
+            raise ValueError(f"Unexpected value error: {handle}")
+        if res is None:
+            return None
+        if isinstance(res, Failure):
+            raise TerminalError(res.message, res.code)
+        return res
+
+    async def create_poll_or_cancel_coroutine(self, handle) -> bytes | None:
+        """Create a coroutine to poll the handle."""
+        await self.take_and_send_output()
         while True:
-            res = self.vm.take_async_result(handle)
-            if isinstance(res, NotReady):
+            if self.vm.is_completed(handle):
+                # Handle is completed
+                return self.must_take_notification(handle)
+
+            # Nothing ready yet, let's try to make some progress
+            do_progress_response = self.vm.do_progress([handle, CANCEL_HANDLE])
+            if isinstance(do_progress_response, PyDoProgressAnyCompleted):
+                # One of the handles completed, we can continue
+                continue
+            if isinstance(do_progress_response, PyDoProgressCancelSignalReceived):
+                # Raise cancel signal
+                raise TerminalError("cancelled", 409)
+            if isinstance(do_progress_response, PyDoProgressReadFromInput):
+                # We need to read from input
                 chunk = await self.receive()
                 if chunk.get('body', None) is not None:
                     assert isinstance(chunk['body'], bytes)
@@ -187,14 +213,13 @@ class ServerInvocationContext(ObjectContext):
                 if not chunk.get('more_body', False):
                     self.vm.notify_input_closed()
                 continue
-            if res is None:
-                return None
-            if isinstance(res, Failure):
-                raise TerminalError(res.message, res.code)
-            return res
+            if isinstance(do_progress_response, PyDoProgressExecuteRun):
+                await self.run_coros_to_execute[do_progress_response.handle]
+                await self.take_and_send_output()
+
 
     def get(self, name: str, serde: Serde[T] = JsonSerde()) -> typing.Awaitable[Optional[Any]]:
-        coro = self.create_poll_coroutine(self.vm.sys_get_state(name))
+        coro = self.create_poll_or_cancel_coroutine(self.vm.sys_get_state(name))
 
         async def await_point():
             """Wait for this handle to be resolved."""
@@ -206,7 +231,7 @@ class ServerInvocationContext(ObjectContext):
         return await_point() # do not await here, the caller will do it.
 
     def state_keys(self) -> Awaitable[List[str]]:
-        return self.create_poll_coroutine(self.vm.sys_get_state_keys()) # type: ignore
+        return self.create_poll_or_cancel_coroutine(self.vm.sys_get_state_keys()) # type: ignore
 
     def set(self, name: str, value: T, serde: Serde[T] = JsonSerde()) -> None:
         """Set the value associated with the given name."""
@@ -227,37 +252,23 @@ class ServerInvocationContext(ObjectContext):
             body=self.invocation.input_buffer,
         )
 
-    # pylint: disable=W0236
-    # pylint: disable=R0914
-    async def run(self,
-                  name: str,
-                  action: Callable[[], T] | Callable[[], Awaitable[T]],
-                  serde: Optional[Serde[T]] = JsonSerde(),
-                  max_attempts: Optional[int] = None,
-                  max_retry_duration: Optional[timedelta] = None) -> T:
-        assert serde is not None
-        res = self.vm.sys_run_enter(name)
-        if isinstance(res, Failure):
-            raise TerminalError(res.message, res.code)
-        if isinstance(res, bytes):
-            return cast(T, serde.deserialize(res))
-        # the side effect was not executed before, so we need to execute it now
-        assert res is None
+    async def create_run_coroutine(self,
+                                   handle: int,
+                                   action: Callable[[], T] | Callable[[], Awaitable[T]],
+                                   serde: Serde[T],
+                                   max_attempts: Optional[int] = None,
+                                   max_retry_duration: Optional[timedelta] = None):
+        """Create a coroutine to poll the handle."""
         try:
             if inspect.iscoroutinefunction(action):
                 action_result = await action() # type: ignore
             else:
                 action_result = action()
             buffer = serde.serialize(action_result)
-            handle = self.vm.sys_run_exit_success(buffer)
-            await self.create_poll_coroutine(handle)
-            return action_result
+            self.vm.propose_run_completion_success(handle, buffer)
         except TerminalError as t:
             failure = Failure(code=t.status_code, message=t.message)
-            handle = self.vm.sys_run_exit_failure(failure)
-            await self.create_poll_coroutine(handle)
-            # unreachable
-            assert False
+            self.vm.propose_run_completion_failure(handle, failure)
         # pylint: disable=W0718
         except Exception as e:
             if max_attempts is None and max_retry_duration is None:
@@ -266,17 +277,38 @@ class ServerInvocationContext(ObjectContext):
             failure = Failure(code=500, message=str(e))
             max_duration_ms = None if max_retry_duration is None else int(max_retry_duration.total_seconds() * 1000)
             config = RunRetryConfig(max_attempts=max_attempts, max_duration=max_duration_ms)
-            exit_handle = self.vm.sys_run_exit_transient(failure=failure, attempt_duration_ms=1, config=config)
-            if exit_handle is None:
-                raise e from None # avoid the traceback that says exception was raised while handling another exception
-            await self.create_poll_coroutine(exit_handle)
-            # unreachable
-            assert False
+            self.vm.propose_run_completion_transient(handle, failure=failure, attempt_duration_ms=1, config=config)
+
+    # pylint: disable=W0236
+    # pylint: disable=R0914
+    def run(self,
+                  name: str,
+                  action: Callable[[], T] | Callable[[], Awaitable[T]],
+                  serde: Optional[Serde[T]] = JsonSerde(),
+                  max_attempts: Optional[int] = None,
+                  max_retry_duration: Optional[timedelta] = None) -> Awaitable[T]:
+        assert serde is not None
+        handle = self.vm.sys_run(name)
+
+        # Register closure to run
+        self.run_coros_to_execute[handle] = self.create_run_coroutine(handle, action, serde, max_attempts, max_retry_duration)
+
+        # Prepare response coroutine
+        coro = self.create_poll_or_cancel_coroutine(handle)
+        async def await_point():
+            """Wait for this handle to be resolved."""
+            res = await coro
+            if res is None:
+                return None
+            return serde.deserialize(res)
+
+        return await_point() # do not await here, the caller will do it.
+
 
     def sleep(self, delta: timedelta) -> Awaitable[None]:
         # convert timedelta to milliseconds
         millis = int(delta.total_seconds() * 1000)
-        return self.create_poll_coroutine(self.vm.sys_sleep(millis)) # type: ignore
+        return self.create_poll_or_cancel_coroutine(self.vm.sys_sleep(millis)) # type: ignore
 
     def do_call(self,
                 tpe: Callable[[Any, I], Awaitable[O]],
@@ -319,10 +351,10 @@ class ServerInvocationContext(ObjectContext):
 
         async def await_point(s: ServerInvocationContext, h, o: Serde[O]):
             """Wait for this handle to be resolved, and deserialize the response."""
-            res = await s.create_poll_coroutine(h)
+            res = await s.create_poll_or_cancel_coroutine(h)
             return o.deserialize(res) # type: ignore
 
-        return await_point(self, handle, output_serde)
+        return await_point(self, handle.result_handle, output_serde)
 
     def service_call(self,
                      tpe: Callable[[Any, I], Awaitable[O]],
@@ -368,7 +400,7 @@ class ServerInvocationContext(ObjectContext):
                   serde: typing.Optional[Serde[I]] = JsonSerde()) -> typing.Tuple[str, Awaitable[Any]]:
         assert serde is not None
         name, handle = self.vm.sys_awakeable()
-        coro = self.create_poll_coroutine(handle)
+        coro = self.create_poll_or_cancel_coroutine(handle)
 
         async def await_point():
             """Wait for this handle to be resolved."""
